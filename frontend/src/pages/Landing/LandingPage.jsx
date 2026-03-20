8deee8878e9a0095f3f6c5dd4e8f238348efbe82
Inhouse-Internships <inhouseinternships@adityauniversity.in>
2026-03-20 12:38:09 +0530

    TESTING

--- frontend/src/pages/Landing/LandingPage.jsx
+++ frontend/src/pages/Landing/LandingPage.jsx
@@ -437,7 +437,7 @@ export default function LandingPage() {
                                 }
                             }}
                         >
-                            ADITYA UNIVERSITY
+                            ADITYA UNIVERSITY PRESENTS
                         </Typography>
                         <Typography
                             variant="h1"
